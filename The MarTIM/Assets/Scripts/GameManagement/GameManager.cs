1cb604b3add35cfba8c471f75a8b94b212618a67
James Kearney <jkearney2013@my.fit.edu>
2018-03-29 11:33:18 -0400

    Project version updated with basic folders

---
+++ The MarTIM/Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStates
+{
+    LoadingScreen,
+    MainMenu
+}
+public delegate void OnStateChangeHandler();
+
+public class GameManager : MonoSingleton<GameManager> {
+   
+ 
+    public event OnStateChangeHandler OnStateChange;
+   
+
+    public GameStates GameState { get; private set; }
+    // Use this for initialization
+    void Start () {
+        
+    }
+	public void LoadScene()
+    {
+
+    }
+
+    public void SetGameState(GameStates gameState)
+    {
+        this.GameState = gameState;
+        if (OnStateChange != null)
+        {
+            OnStateChange();
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+ 
+}
---
+++ The MarTIM/Assets/Scripts/Utility/MonoSingleton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    static T s_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            return s_instance ?? new GameObject(typeof(T).Name).AddComponent<T>();
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (s_instance)
+        {
+            Debug.LogWarning("A Singleton instance was previously initialized.");
+            Destroy(gameObject);
+            return;
+        }
+
+        s_instance = GetComponent<T>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    protected virtual void OnEnable()
+    {
+        if (!s_instance)
+            s_instance = GetComponent<T>();
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (s_instance != GetComponent<T>())
+            return;
+        
+
+        s_instance = null;
+    }
+}
